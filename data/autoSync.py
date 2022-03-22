abf06e79dc990af9fe6204c5f2accd3b31497a93
Stephen Shaw <stephen.shaw120@gmail.com>
2022-03-22 10:02:06 -0500

    Delete autoSync.py
    Deleting from data directory. This is stored in top level of HB folder and was removed from data folder in a review of the system file structures.

--- data/autoSync.py
+++
@@ -1,8 +0,0 @@
-from subprocess import Popen
-import time
-
-while (True):
-    p = Popen("lol.bat", cwd=r"C:\Users\bd1912wh\Desktop\data")
-    stdout, stderr = p.communicate()
-    print ("cool")
-    #time.sleep(2)
